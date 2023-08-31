d6bd58a45b43b9b0f049735e687ef1fc9f0a64a7
Shnayderhust <125040759+Shnayderhust@users.noreply.github.com>
2023-08-31 10:22:54 +0300

    db and home page updates

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/index.css">
+    <title><?= $appname = 'Unigram' ?> | Secure and Reliable</title>
+</head>
+<body>
+    <header>
+        <div class="logo"><h1><?= $appname = 'Unigram' ?></h1></div>
+        <ul>
+            <li>Features</li>
+            <li>Privacy</li>
+            <li>Help Center</li>
+        </ul>
+        <div class="reg">
+            <button>Log In</button>
+            <button>Register</button>
+        </div>
+    </header>
+
+    <main>
+        <p>Simple, reliable, private messaging with fellow university colleagues</p>
+
+        <p>Hang out anytime, anywhere</p>
+        <p>Unigram makes it easy and fun to stay close to your favorite people.</p>
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+</html>
