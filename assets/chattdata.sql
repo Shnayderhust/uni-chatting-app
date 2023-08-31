124d250e21ad7908cc0d0fc3311a8853951ba34f
Shnayderhust <125040759+Shnayderhust@users.noreply.github.com>
2023-08-31 10:42:24 +0300

    db update

--- assets/chattdata.sql
+++ assets/chattdata.sql
@@ -1,41 +1,4 @@
-CREATE TABLE `chat` (
-  `id` int(11) NOT NULL PRIMARY KEY,
-  `user_id` int(11) NOT NULL,
-  `user_name` varchar(250) NOT NULL,
-  `message` text NOT NULL,
-  `dateposted` varchar(200) NOT NULL
-)
-
-CREATE TABLE `department` (
-  `id` int(11) NOT NULL PRIMARY KEY,
-  `department` varchar(200) NOT NULL,
-  `faculty` varchar(200) NOT NULL
-)
-
-CREATE TABLE `faculty` (
-  `id` int(11) NOT NULL PRIMARY KEY,
-  `faculty` varchar(300) NOT NULL
-)
-
-CREATE TABLE `dm` (
-  `id` int(11) NOT NULL PRIMARY KEY,
-  `sender_id` int(11) NOT NULL,
-  `reciever_id` int(11) NOT NULL,
-  `message` text NOT NULL,
-  `dateposted` varchar(250) NOT NULL
-)
-
-CREATE TABLE `inbox` (
-  `id` int(11) NOT NULL PRIMARY KEY,
-  `user_id` int(11) NOT NULL,
-  `sender_id` int(11) NOT NULL,
-  `sender_name` varchar(200) NOT NULL,
-  `receiver_name` varchar(250) NOT NULL,
-  `message` text NOT NULL,
-  `dateposted` varchar(250) NOT NULL,
-  `time` varchar(250) NOT NULL
-)
-
+-- @block
 CREATE TABLE `users` (
   `id` int(11) NOT NULL PRIMARY KEY,
   `firstname` varchar(200) NOT NULL,
@@ -51,11 +14,51 @@ CREATE TABLE `users` (
   `account_type` varchar(250) NOT NULL DEFAULT 'user'
 )
 
-CREATE TABLE `user_online` (
-  `id` int(11) NOT NULL PRIMARY KEY,
-  `session` char(100) NOT NULL DEFAULT '',
+-- @block
+CREATE TABLE `conversation` (
+  `conver_id` int(11) NOT NULL PRIMARY KEY,
   `user_id` int(11) NOT NULL,
-  `name` varchar(250) NOT NULL,
-  `profile_pic` varchar(250) NOT NULL,
-  `time` int(11) NOT NULL DEFAULT '0'
+  `user_name` varchar(250) NOT NULL,
+  `message` text NOT NULL,
+  `dateposted` varchar(200) NOT NULL
 )
+
+-- @block
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL PRIMARY KEY,
+  `sender_id` int(11) NOT NULL,
+  `reciever_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `dateposted` varchar(250) NOT NULL
+)
+
+CREATE TABLE `university` (
+  `id` int(11) NOT NULL PRIMARY KEY,
+  `university` varchar(200) NOT NULL,
+)
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL PRIMARY KEY,
+  `course` varchar(300) NOT NULL
+)
+
+
+-- CREATE TABLE `inbox` (
+--   `id` int(11) NOT NULL PRIMARY KEY,
+--   `user_id` int(11) NOT NULL,
+--   `sender_id` int(11) NOT NULL,
+--   `sender_name` varchar(200) NOT NULL,
+--   `receiver_name` varchar(250) NOT NULL,
+--   `message` text NOT NULL,
+--   `dateposted` varchar(250) NOT NULL,
+--   `time` varchar(250) NOT NULL
+-- )
+
+-- CREATE TABLE `user_online` (
+--   `id` int(11) NOT NULL PRIMARY KEY,
+--   `session` char(100) NOT NULL DEFAULT '',
+--   `user_id` int(11) NOT NULL,
+--   `name` varchar(250) NOT NULL,
+--   `profile_pic` varchar(250) NOT NULL,
+--   `time` int(11) NOT NULL DEFAULT '0'
+-- )
