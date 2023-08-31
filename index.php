430a0291e1dccc0a4504f07617f168c8aaed0f2d
Shnayderhust <125040759+Shnayderhust@users.noreply.github.com>
2023-09-01 01:43:02 +0300

    updates

--- index.php
+++ index.php
@@ -3,17 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./assets/pics and icons/favicon.png">
     <link rel="stylesheet" href="./assets/index.css">
-    <title><?= $appname = 'Unigram' ?> | Secure and Reliable</title>
+    <title>Unigram | Secure and Reliable</title>
 </head>
 <body>
     <header>
-        <div class="logo"><h1><?= $appname = 'Unigram' ?></h1></div>
+        <div class="logo">
+            <!-- <img src="./assets/pics and icons/favicon.png" alt="logo" style="width: 2rem; height: 2rem;"> -->
+            <h1>Unigram</h1>
+        </div>
+
         <ul>
             <li>Features</li>
             <li>Privacy</li>
             <li>Help Center</li>
         </ul>
+
         <div class="reg">
             <button>Log In</button>
             <button>Register</button>
@@ -21,6 +27,7 @@
     </header>
 
     <main>
+        <img src="./assets/pics and icons/avatar.jpg" alt="pic">
         <p>Simple, reliable, private messaging with fellow university colleagues</p>
 
         <p>Hang out anytime, anywhere</p>
